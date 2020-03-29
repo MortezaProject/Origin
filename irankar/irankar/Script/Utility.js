a1fe6008b804dcbc3f25c3722c8c7e4285bdd087
hamraz <alizareh478@gmail.com>
2020-03-28 23:44:43 -0700

    change function HideDescriptionBlog

--- irankar/irankar/Script/Utility.js
+++ irankar/irankar/Script/Utility.js
@@ -1,6 +1,7 @@
 ﻿
 ShowDescriptionBlog = function (param) {
     document.querySelector('#description-blog span').textContent = param.getAttribute('desc');
+    document.querySelector('#dateblog span').textContent = param.getAttribute('dateblog');
     $("#description-blog").removeClass("d-none");
     for (var i = 0; i < $("#SwiperSlider").children().length; i++) {
         if ($("#SwiperSlider").children()[i].textContent != param.textContent )
@@ -10,7 +11,7 @@ ShowDescriptionBlog = function (param) {
 
 HideDescriptionBlog = function (param) {
     //$("#description-blog").removeClass("d-none");
-    $("#description-blog").addClass("d-none")
+    //$("#description-blog").addClass("d-none")
     for (var i = 0; i < $("#SwiperSlider").children().length; i++) {
             $("#SwiperSlider").children()[i].style.opacity = "1";
     }
@@ -31,44 +32,51 @@ CreateBlogHome = function () {
     let data = [{	
         src: "../Images/img1.jpg",	
         title: "1قرآن از نظر دانشمندان غربی",	
-        desc: "متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1"	
+        desc: "متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1"	,
+        date:"جمعه23خرداد"
     },	
                    {	
                        src: "../Images/img1.jpg",	
                        title: "2قرآن از نظر دانشمندان غربی",	
-                       desc: "متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2"	
+                       desc: "متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2",
+                       date:"جمعه24خرداد"
                    },	
                    {	
                        src: "../Images/img1.jpg",	
                        title: "3قرآن از نظر دانشمندان غربی",	
-                       desc: "متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3"	
+                       desc: "متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3",
+                       date:"جمعه25خرداد"
                    },	
                    {	
                        src: "../Images/img1.jpg",	
                        title: "4قرآن از نظر دانشمندان غربی",	
-                       desc: "متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4"	
+                       desc: "متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4",
+                       date:"جمعه26خرداد"
                    },	
                    {	
                        src: "../Images/img1.jpg",	
                        title: "5قرآن از نظر دانشمندان غربی",	
-                       desc: "متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5"	
+                       desc: "متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5",
+                       date:"جمعه27خرداد"
                    },	
                    {	
                        src: "../Images/img1.jpg",	
                        title: "6قرآن از نظر دانشمندان غربی",	
-                       desc: "متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6"	
+                       desc: "متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6",
+                       date:"جمعه28خرداد"
                    },	
                    {	
                        src: "../Images/img1.jpg",	
                        title: "7قرآن از نظر دانشمندان غربی",	
-                       desc: "متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7"	
+                       desc: "متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7",
+                       date:"جمعه29خرداد"
                    },	
     ]	
     for (let i = 0; i < data.length; i++) {	
         let div = document.createElement('div');	
         div.classList.add('swiper-slide');
         div.classList.add('swiper-slide-cust');
-        div.innerHTML = '<div class="view zoom" desc=' + data[i].desc +	
+        div.innerHTML = '<div class="view zoom" desc=' + data[i].desc +' dateblog='+ data[i].date +
             ' onmouseover="ShowDescriptionBlog(this)" onmouseout="HideDescriptionBlog(this)"><div class="item-blog d-flex flex-column justify-content-between"><div><img src=' +	
             data[i].src +	
             ' class="blog-image" /></div><div class="p-2 text-center item-blog-desc"><p>' + data[i]	
