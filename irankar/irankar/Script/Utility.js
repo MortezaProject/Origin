e9c1ea016783ed05f8fab7d8fd054010036786f3
hamraz <alizareh478@gmail.com>
2020-04-05 10:11:53 -0700

    change function loadcommment

--- irankar/irankar/Script/Utility.js
+++ irankar/irankar/Script/Utility.js
@@ -31,50 +31,8 @@ ChangeIconCollapseChild = function (e) {
 OpenDilogComment=function(e){
     debugger;
 }
-CreateBlogHome = function () {
-    let data = [{	
-        src: "../Images/img1.jpg",	
-        title: "1قرآن از نظر دانشمندان غربی",	
-        desc: "متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1متن1"	,
-        date:"جمعه23خرداد"
-    },	
-                   {	
-                       src: "../Images/img1.jpg",	
-                       title: "2قرآن از نظر دانشمندان غربی",	
-                       desc: "متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2متن2",
-                       date:"جمعه24خرداد"
-                   },	
-                   {	
-                       src: "../Images/img1.jpg",	
-                       title: "3قرآن از نظر دانشمندان غربی",	
-                       desc: "متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3متن3",
-                       date:"جمعه25خرداد"
-                   },	
-                   {	
-                       src: "../Images/img1.jpg",	
-                       title: "4قرآن از نظر دانشمندان غربی",	
-                       desc: "متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4متن4",
-                       date:"جمعه26خرداد"
-                   },	
-                   {	
-                       src: "../Images/img1.jpg",	
-                       title: "5قرآن از نظر دانشمندان غربی",	
-                       desc: "متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5متن5",
-                       date:"جمعه27خرداد"
-                   },	
-                   {	
-                       src: "../Images/img1.jpg",	
-                       title: "6قرآن از نظر دانشمندان غربی",	
-                       desc: "متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6متن6",
-                       date:"جمعه28خرداد"
-                   },	
-                   {	
-                       src: "../Images/img1.jpg",	
-                       title: "7قرآن از نظر دانشمندان غربی",	
-                       desc: "متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7متن7",
-                       date:"جمعه29خرداد"
-                   },	
-    ]	
+CreateBlogHome = function (data) {
+
     for (let i = 0; i < data.length; i++) {	
         let div = document.createElement('div');	
         div.classList.add('swiper-slide');
@@ -118,46 +76,7 @@ CreateBlogHome = function () {
     });	
 
 }
-LoadCommentsUsers =function(){
-    debugger;
-    let data_comments=[
-        {
-            commentid:"1",
-            parent_comment_id:"",
-            comment:"نحوه دسترسی به این منابع چگونه است؟",
-            comment_sender_name:"مهسا:",
-            date:"9:12- 29 شهریور 98"
-        },
-            {
-                commentid:"2",
-                parent_comment_id:"1",
-                comment:"نحوه دسترسی به این منابع چگونه است؟",
-                comment_sender_name:"ادمین:",
-                date:"9:12- 29 شهریور 98"
-            },
-                {
-                    commentid:"3",
-                    parent_comment_id:"",
-                    comment:"نحوه دسترسی به این منابع چگونه است؟",
-                    comment_sender_name:"علی:",
-                    date:"9:12- 29 شهریور 98"
-                },
-                        {
-                            commentid:"5",
-                            parent_comment_id:"4",
-                            comment:"نحوه دسترسی به این منابع چگونه است؟",
-                            comment_sender_name:"علی:",
-                            date:"9:12- 29 شهریور 98"
-                        },
-                    {
-                        commentid:"4",
-                        parent_comment_id:"3",
-                        comment:"لطفا برای اطلاع دقیق در ساعات اداری با شماره های مرکز تماس بگیرید",
-                        comment_sender_name:"ادمین:",
-                        date:"9:12- 29 شهریور 98"
-                    }
-
-    ]
+LoadCommentsUsers =function(data_comments){
     data_comments.sort((a, b) => (a.parent_comment_id > b.parent_comment_id) ? 1 : -1)
     for (var i = 0; i < data_comments.length; i++) {
         if(data_comments[i].parent_comment_id =="")
